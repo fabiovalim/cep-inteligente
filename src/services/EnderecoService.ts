d3984f30e3d261ca87c42d10a6b20359fd5e3504
Fabio Valim <oficialvalim@gmail.com>
2025-10-16 11:20:26 -0300

    feat: criando o service de buscar cep pelo endereco

--- src/services/EnderecoService.ts
+++ src/services/EnderecoService.ts
@@ -10,3 +10,12 @@ const getByCep = async (cep: number): Promise<Endereco | ApiException> => {
     return new ApiException(error.message || "Erro ao buscar endereço...")
   }
 };
+
+const getByEndereco = async (uf: string, cidade: string, logr: string): Promise<Endereco | ApiException> => {
+  try {
+    const { data } = await Api().get(`/${uf}/${cidade}/${logr}/json`);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao buscar endereço...")
+  }
+};
