9cc4a74d9ed379a857e9b8c8f089f8682df9a5ec
Fabio Valim <oficialvalim@gmail.com>
2025-10-16 11:25:58 -0300

    refactor: alterando o local de uso do header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Footer } from "./components/layout/Footer";
-import { Header } from "./components/layout/Header";
 import { Routes } from "./routes";
 
 export const App = () => {
   return (
     <div>
-      <Header />
+      
       <Routes />
-      <Footer />
+      
     </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,17 +2,23 @@ import { SecaoInicio } from '../../components/SecaoInicio';
 import { SecaoComoFunciona } from '../../components/SecaoComoFunciona';
 import { SecaoSobreApi } from '../../components/SecaoSobreApi';
 import { SecaoSobreMim } from '../../components/SecaoSobreMim';
+import { Header } from '../../components/layout/Header';
+import { Footer } from '../../components/layout/Footer';
 
 export const Home = () => {
   return (
     <main>
+      <Header />
+
       <SecaoInicio />
 
       <SecaoComoFunciona />
 
       <SecaoSobreApi />
 
-      <SecaoSobreMim />     
+      <SecaoSobreMim />
+
+      <Footer /> 
     </main>
   );
 };
