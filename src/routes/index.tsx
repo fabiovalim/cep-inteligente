8102ae6eba8aae58215036607d7067b09cdc7ca3
Fabio Valim <oficialvalim@gmail.com>
2025-10-18 16:51:14 -0300

    feat: adicionando rota para buscar cep

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom"
 import { Home } from "../pages/home/Home";
+import { BuscaCepPage } from "../pages/consultaIndividual/ConsultaIndividual";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" element={<Home />} />
+        <Route path="/buscar-cep" element={<BuscaCepPage />} />
       </Switch>
     </BrowserRouter>
   );
