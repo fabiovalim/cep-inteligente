79eadae0d5177fd0511fc90b2cda6a13d8968632
Fabio Valim <oficialvalim@gmail.com>
2025-10-26 10:25:14 -0300

    fix: atualizando logo na navbar das páginas

--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -5,6 +5,9 @@ export const HeaderPage = () => {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-page">
       <div className="container-fluid">
+        <div className="fw-bold navbar-brand icon-nav-page">
+          <p>Zetta Lab 2025</p>
+        </div>
         <div className="ms-auto">
           <Link to="/"><BoxArrowInLeft size={30} className='me-2' /></Link>
         </div>
